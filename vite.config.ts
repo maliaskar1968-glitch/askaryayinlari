1e445e2bb98617dc0b5bbdfa9fa0f55d01c4c747
maliaskar1968-glitch <maliaskar.1968@gmail.com>
2026-08-14 16:40:23 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+   return {
+    publicDir: 'halk',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
